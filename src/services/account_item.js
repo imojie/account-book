e1d82dcb9011233ada10d1ea90f4d9635145b53a
Mr_Jing <admin@imojie.com>
2017-11-22 20:26:16 +0800

    保存收入

--- src/services/account_item.js
+++ src/services/account_item.js
@@ -72,10 +72,59 @@ function saveExpenditure(accountItem) {
     });
 }
 
+function saveIncome(accountItem) {
+    const accessToken = getAccessToken();
+
+    let instance = axios.create({
+        baseURL: 'http://account-book.app/api/',
+        timeout: 2000,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let postData = {
+        account_book_id: 22,
+        type: accountItem.type,
+        amount: accountItem.amount,
+        to_account_id: accountItem.account,
+        category_id: accountItem.income_category,
+        remark: accountItem.remark,
+        occurred_at: moment(accountItem.occurred_at).format('YYYY-MM-DD HH:mm:ss')
+    };
+
+    let config = {
+        url: "account-items",
+        method: "post",
+        data: postData
+    };
+
+    if (accountItem.id) {
+        config = {
+            url: `account-items/${accountItem.id}`,
+            method: "put",
+            data: postData
+        }
+    }
+
+    return instance.request(config).then(function (response) {
+        return response.data;
+    }).catch(function (response) {
+        if (response instanceof Error) {
+            console.log('Error', response.message, response.response);
+        } else {
+            console.log(response);
+        }
+    });
+}
+
 export function saveAccountItem(accountItem) {
     console.log(typeof accountItem.type);
     switch (accountItem.type) {
         case '1':
+        case 1:
+            return saveIncome(accountItem);
             break;
         case '2':
         case 2:
