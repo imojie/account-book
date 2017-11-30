0e860867d82749741c5fc12d7611071806de17cf
Mr_Jing <admin@imojie.com>
2017-11-30 22:42:03 +0800

    保存“借贷”

--- src/services/account_item.js
+++ src/services/account_item.js
@@ -168,6 +168,54 @@ function saveTransfer(accountItem) {
 }
 
 
+function saveLoan(accountItem) {
+    const accessToken = getAccessToken();
+
+    let instance = axios.create({
+        baseURL: 'http://account-book.app/api/',
+        timeout: 2000,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let postData = {
+        account_book_id: 22,
+        type: accountItem.type,
+        amount: accountItem.amount,
+        from_account_id: accountItem.loan_from_account,
+        to_account_id: accountItem.loan_to_account,
+        category_id: accountItem.loan_category,
+        remark: accountItem.remark,
+        occurred_at: moment(accountItem.occurred_at).format('YYYY-MM-DD HH:mm:ss')
+    };
+
+    let config = {
+        url: "account-items",
+        method: "post",
+        data: postData
+    };
+
+    if (accountItem.id) {
+        config = {
+            url: `account-items/${accountItem.id}`,
+            method: "put",
+            data: postData
+        }
+    }
+
+    return instance.request(config).then(function (response) {
+        return response.data;
+    }).catch(function (response) {
+        if (response instanceof Error) {
+            console.log('Error', response.message, response.response);
+        } else {
+            console.log(response);
+        }
+    });
+}
+
 export function saveAccountItem(accountItem) {
     console.log(typeof accountItem.type);
     switch (accountItem.type) {
@@ -181,7 +229,8 @@ export function saveAccountItem(accountItem) {
         case 3:
             return saveTransfer(accountItem);
         case '4':
-            break;
+        case 4:
+            return saveLoan(accountItem);
         default:
             break;
     }
